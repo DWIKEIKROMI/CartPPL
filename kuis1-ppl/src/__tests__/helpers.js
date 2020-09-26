a010a57626ac0f2606b285bdac11920208a2c46c
Dwike Ikromi <dwikeikromi1205@gmail.com>
2020-09-26 19:47:06 +0700

    pengurangan

--- kuis1-ppl/src/__tests__/helpers.js
+++ kuis1-ppl/src/__tests__/helpers.js
@@ -2,3 +2,8 @@
     Gunakan file ini untuk membuat test case dengan Jest
 */
 
+const helper = require('../helpers');
+
+test('Jika qty adalah 5, ketika dikurangi 1 jadi = 4', () => {
+    expect(helper.decrementQty(2)).toBe(1);
+});
