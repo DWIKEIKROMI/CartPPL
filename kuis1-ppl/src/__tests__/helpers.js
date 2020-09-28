5162e5c86f71b079d051ca44222245d47a5eb60e
Dwike Ikromi <dwikeikromi1205@gmail.com>
2020-09-28 10:13:31 +0700

    membuat tombol - tidak menjadi 0 dan minus

--- kuis1-ppl/src/__tests__/helpers.js
+++ kuis1-ppl/src/__tests__/helpers.js
@@ -11,3 +11,6 @@ test('Jika qty adalah 2, ketika dikurangi 1 jadi = 1', () => {
 test('Jika qty adalah 1, ketika ditambah 1 jadi = 2', () => {
     expect(helper.incrementQty(1)).toBe(2);
 });
+test('Jika qtynya adalah 1, maka tombol - tidak akan berkurang menjadi 0 dan minus', () => {
+    expect(helper.disableDecrement(1)).toBeTruthy();
+});
