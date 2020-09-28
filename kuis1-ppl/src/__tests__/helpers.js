a380f40873a2c7f1b8de96ead23d23c186934804
Dwike Ikromi <dwikeikromi1205@gmail.com>
2020-09-28 09:43:38 +0700

    menambah increment testing

--- kuis1-ppl/src/__tests__/helpers.js
+++ kuis1-ppl/src/__tests__/helpers.js
@@ -4,6 +4,10 @@
 
 const helper = require('../helpers');
 
-test('Jika qty adalah 5, ketika dikurangi 1 jadi = 4', () => {
+test('Jika qty adalah 2, ketika dikurangi 1 jadi = 1', () => {
     expect(helper.decrementQty(2)).toBe(1);
+});
+
+test('Jika qty adalah 1, ketika ditambah 1 jadi = 2', () => {
+    expect(helper.incrementQty(1)).toBe(2);
 });
